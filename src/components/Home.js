0543df7f36586cf0b03bdc08c872a8ce5d720cac
Maria Barkouzou <m.mparkouzou@yahoo.gr>
2021-10-04 15:20:03 +0300

    Fixing responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <main>
       <img src={image} alt="mountains" className="absolute h-full object-cover w-full"/>
-      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+      <section className="about-section relative flex justify-center pt-12 lg:pt-64 px-8">
         <h1 className="text-6xl text-white font-bold cursive leading-none lg:leading-slug home-name">Hi!I'm Maria.</h1>
       </section>
     </main>
